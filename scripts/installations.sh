4c0c3045c5b1149aab75e61792c663a930d532c2
chetan <chetansharma.019@gmail.com>
2020-06-18 16:19:28 +0200

    test for mac

--- scripts/installations.sh
+++ scripts/installations.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 # Intall nodejs, npm package.json
-apt-get update -y && apt-get install -y nodejs npm
+curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
 npm install
