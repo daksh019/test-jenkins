16dec34def5060b8a907034e595e460bd2da0f23
chetan <chetansharma.019@gmail.com>
2020-06-18 16:31:47 +0200

    test for mac brew

--- scripts/installations.sh
+++ scripts/installations.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 # Intall nodejs, npm package.json
-curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+brew install node
 npm install
